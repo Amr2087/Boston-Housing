b135938c4107da6d76d036b6e0cc4a276088bd54
Amr2087 <82404978+Amr2087@users.noreply.github.com>
2022-10-29 08:45:53 +0200

    change to SVR

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-dataset = pd.read_csv('data/housing.csv')
-X = dataset.iloc[:, 0:3].values
+# Importing the dataset
+dataset = pd.read_csv('data/boston_train.csv')
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+y = y.reshape(len(y),1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-# poly_reg = PolynomialFeatures(degree=4)
-# X_poly = poly_reg.fit_transform(X_train)
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-#
-# regressor = LinearRegression()
-# regressor.fit(X, y)
-y_pred = regressor.predict(X_test)
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+y_train = sc_y.fit_transform(y_train)
 
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import accuracy_score
-accuracy_score = regressor.score(X, y)
-print(accuracy_score)
+# Training the SVR model on the Training set
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X_train, np.ravel(y_train))
 
-plt.scatter(y_test, y_pred)
-plt.xlabel("Prices")
-plt.ylabel("Predicted prices")
-plt.title("Prices vs Predicted prices")
-plt.show()
+# Predicting the Test set results
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(X_test)).reshape(-1, 1))
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Evaluating the Model Performance
+from sklearn.metrics import r2_score
+acc_score = r2_score(y_test, y_pred)
+print(acc_score)
+
+
